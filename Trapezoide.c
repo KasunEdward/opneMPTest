224159f5be764fce5e4917b5816a2f8800be93f2
Kasun Edward <kasunedward48@gmail.com>
2017-07-06 03:38:20 +0530

    Trapezoide code was added

---
+++ Trapezoide.c
@@ -0,0 +1,50 @@
+//
+// Created by kasun on 7/6/17.
+//
+#include<stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+void trap(double a, double b,int n,double* global_result_p);
+double f(double a);
+int main(int argc,char* argv[]){
+    double global_result=0.0;
+    double a,b;
+    int n;
+    int thread_count;
+
+    thread_count=atoi(argv[1]);
+    printf("Enter a,b and n\n");
+//    printf("%d",thread_count);
+    scanf("%lf %lf %d",&a,&b,&n);
+#pragma omp parallel num_threads(thread_count)
+    trap(a,b,n,&global_result);
+
+printf("Result=%lf",global_result);
+return 0;
+}
+
+void trap(double a, double b,int n,double* global_result_p){
+    double h,x,my_result;
+    double local_a,local_b;
+    int i,local_n;
+    int my_rank=omp_get_thread_num();
+    int thread_count=omp_get_num_threads();
+
+    h=(b-a)/n;
+    local_n=n/thread_count;
+    local_a=a + my_rank*local_n*h;
+    local_b=b+local_n*h;
+    my_result=(f(local_a)+f(local_b))/2.0;
+    for(i=1;i<=local_n-1;i++){
+        x=local_a + i*h;
+        my_result+=f(x);
+    }
+    my_result=my_result*h;
+
+#pragma omp citical
+    *global_result_p+=my_result;
+
+}
+ double f(double a){
+     return a*a;
+ }
